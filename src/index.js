e81c3510277434da8a663e361c4a04253382278a
MoSavvyCodes <116381882+MoSavvyCodes@users.noreply.github.com>
2024-08-11 11:03:57 -0400

    added Javascript to search bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+function searchCity(city) {
+  let apiKey = "tea83c52bbf0734d4o786a9f0ea43910";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
+
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
