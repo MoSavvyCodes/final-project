76991eda8e1aff39b0d18bce2290c0a394664862
MoSavvyCodes <116381882+MoSavvyCodes@users.noreply.github.com>
2024-08-15 19:29:08 -0400

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.temperature.current;
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 function searchCity(city) {
   let apiKey = "tea83c52bbf0734d4o786a9f0ea43910";
@@ -12,10 +16,11 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
-
   cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("New York");
