812b542f1664daeae35f7bda9be5232881bdc0e5
MoSavvyCodes <116381882+MoSavvyCodes@users.noreply.github.com>
2024-09-24 18:41:16 -0400

    Added the humidity and wind speed element

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  console.log(response.data);
 
-  console.log(response.data.condition.description);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = response.data.wind.speed;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 function searchCity(city) {
